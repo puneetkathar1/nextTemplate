035b88ad0b50ca81894ff78dd406f745068bc441
puneetkathar1 <puneetkathar1@gmail.com>
2022-04-01 02:55:08 +0530

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
 }
 
-module.exports = nextConfig
